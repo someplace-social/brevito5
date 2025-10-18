d12bf2d16b9187d8b60a6278ebad0b6d16916e15
sharpie-stories <getg93@gmail.com>
2025-10-18 17:01:40 -0600

    feat: Seed fun_facts table with sample data

---
+++ supabase/migrations/20251018232000_seed_fun_facts.sql
@@ -0,0 +1,6 @@
+INSERT INTO fun_facts (english_text) VALUES
+('A group of flamingos is called a "flamboyance".'),
+('The unicorn is the national animal of Scotland.'),
+('Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.'),
+('A shrimp''s heart is in its head.'),
+('It is impossible for most people to lick their own elbow.');
