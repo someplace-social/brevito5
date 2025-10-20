2179ab904a75a4329e602253cfdda44089b56026
sharpie-stories <getg93@gmail.com>
2025-10-20 15:47:36 -0600

    refactor: remove footer from home page

--- app/page.tsx
+++ app/page.tsx
@@ -186,9 +186,6 @@ export default function Home() {
           )}
         </div>
       </div>
-      <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8">
-        <p>Built with Next.js and Supabase</p>
-      </footer>
     </main>
   );
 }
