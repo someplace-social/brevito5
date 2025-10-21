52e1ecf97d22cc08bcfafbd7c09062b5a6217b79
sharpie-stories <getg93@gmail.com>
2025-10-21 11:38:33 -0600

    config: Add pixabay CDN to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,16 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default withPWA(nextConfig);
