6266931ae8fbaeb62d54285ca84c86cd0a3875c7
sharpie-stories <getg93@gmail.com>
2025-10-21 11:40:24 -0600

    config: Add pixabay.com to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
